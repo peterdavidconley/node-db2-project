ba80998986c61b243b5b5dce8ad9ef73665721b8
Peter Conley <peterdavidconley@gmail.com>
2022-02-15 14:35:00 -0500

    passing 6 tests for GET and POST endpoints

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,13 +1,16 @@
 exports.up = function (knex) {
 
+  return knex.schema.createTable('cars', (tbl) => {
+
   tbl.increments();
   tbl.string('vin').unique().notNullable;
   tbl.string('make').notNullable;
   tbl.string('model').notNullable;
-  tbl.numeric('mileage').notNullable;
+  tbl.decimal('mileage').notNullable;
   tbl.string('title');
   tbl.string('transmission');
-  
+
+  })
 };
 
 exports.down = function (knex) {
